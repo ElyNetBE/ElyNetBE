01b13b1801fa625184bbe209ca63e59fe608e83e
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-01-12 00:40:38 +0700

    Tidak Penting

--- src/item/Spyglass.php
+++ src/item/Spyglass.php
@@ -3,11 +3,6 @@
 namespace pocketmine\item;
 
 use pocketmine\item\Item;
-use pocketmine\item\ItemIdentifier;
-use pocketmine\item\ItemFactory;
-use pocketmine\item\ItemIds;
-use pocketmine\item\VanillaItems;
-use pocketmine\item\StringToItemParser;
 
 class Spyglass extends Item
 {
