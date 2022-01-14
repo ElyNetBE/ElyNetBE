84bd589f520a6fdf0bd40b79b049051ec324b83a
HanifWadowskyy <43465633+HanifWadowskyy@users.noreply.github.com>
2022-01-14 17:21:11 +0000

    New color

--- src/utils/MainLogger.php
+++ src/utils/MainLogger.php
@@ -103,7 +103,7 @@ class MainLogger extends \AttachableThreadedLogger implements \BufferedLogger{
 	}
 
 	public function warning($message){
-		$this->send($message, \LogLevel::WARNING, "WARNING", TextFormat::YELLOW);
+		$this->send($message, \LogLevel::WARNING, "WARNING", TextFormat::BLUE);
 	}
 
 	public function notice($message){
@@ -111,7 +111,7 @@ class MainLogger extends \AttachableThreadedLogger implements \BufferedLogger{
 	}
 
 	public function info($message){
-		$this->send($message, \LogLevel::INFO, "INFO", TextFormat::WHITE);
+		$this->send($message, \LogLevel::INFO, "INFO", TextFormat::AQUA);
 	}
 
 	public function debug($message, bool $force = false){
