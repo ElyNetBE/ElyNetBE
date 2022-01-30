07ff1d08cc662b43afc64811d00a03c96b7039c6
SandhyR <sandhyraihan36@gmail.com>
2022-01-30 20:26:43 +0700

    Command Parameter API

--- src/command/defaults/BanCommand.php
+++ src/command/defaults/BanCommand.php
@@ -27,6 +27,8 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\utils\InvalidCommandSyntaxException;
 use pocketmine\lang\KnownTranslationFactory;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
 use pocketmine\permission\DefaultPermissionNames;
 use pocketmine\player\Player;
 use function array_shift;
@@ -66,4 +68,9 @@ class BanCommand extends VanillaCommand{
 
 		return true;
 	}
+
+    public function getCommandParameter(): array
+    {
+        return [CommandParameter::standard("name", AvailableCommandsPacket::ARG_TYPE_RAWTEXT, 0, true), CommandParameter::standard("reason", AvailableCommandsPacket::ARG_TYPE_RAWTEXT, 1, true)];
+    }
 }
